032362b04f6240d2ecf36e6e61a1158dc4f70077
Matija Mihic <matija@littleemperors.com>
2023-02-11 23:46:10 +0100

    feat (Posts) : added soft delete to Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentTaggable\Taggable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory, Taggable;
+    use HasFactory, Taggable, SoftDeletes;
     
     /**
      * The attributes that are mass assignable.
