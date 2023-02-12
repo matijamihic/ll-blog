905feaed5e67b3830251784a989b62a5b1854048
Matija Mihic <matija@littleemperors.com>
2023-02-12 12:51:02 +0100

    refactor :(Api) : routes

--- routes/api.php
+++ routes/api.php
@@ -10,16 +10,7 @@ use App\Http\Controllers\ProfileController;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
-
-// Route::middleware('auth:jwt')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -29,13 +20,11 @@ Route::controller(AuthController::class)->group(function () {
 });
 
 Route::get('/authors/{id}/posts', [AuthorController::class, 'posts']);
-Route::resource('authors', AuthorController::class, [
+Route::apiResource('authors', AuthorController::class, [
     'only' => ['index', 'show']
 ]);
 
 Route::get('/posts/tags/{tag}/any', [PostController::class, 'withAnyTags']);
 Route::get('/posts/tags/{tag}', [PostController::class, 'withAllTags']);
 Route::get('/posts/tags', [PostController::class, 'tags']);
-Route::resource('posts', PostController::class, [
-    'except' => ['edit', 'create']
-]);
+Route::apiResource('posts', PostController::class);
